d506258426848c7c8d9a3a9a384d94eb8fa3187e
cacfrre <64874614+cacfrre@users.noreply.github.com>
2026-09-08 20:49:51 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ProyectoSeis'
+copyright = '2026, FRRe'
+author = 'CAC'
 
 release = '0.1'
 version = '0.1.0'
